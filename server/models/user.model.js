bebc40461e9c5a48e9fa4042311ec20109faaa26
freefireman <beluh.c.s@gmail.com>
2020-11-11 19:17:30 +0200

    create user.model

---
+++ server/models/user.model.js
@@ -0,0 +1,69 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: 'Name is required'
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 'Email already exists',
+        match: [/.+\@.+\..+/,'Please fill a valid email address'],
+        required: 'Email is required'
+    },
+    created: {
+        type: Date,
+        default: Date.now()
+    },
+    updated: Date,
+    hashed_password: {
+        type: String,
+        required: 'Password is required'
+    },
+    salt: String,
+})
+UserSchema
+    .virtual('password')
+    .set(function (password){
+        this._password = password
+        this.salt = this.makeSalt()
+        this.hashed_password = this.encodePassword(password)
+    })
+    .get(function (){
+        return this._password
+    })
+
+UserSchema.methods = {
+    authenticate: function (plainText){
+        return this.encryptPassword(plainText) === this.hashed_password
+    },
+    encryptedPassword: function (password){
+        if (!password){
+            return ''
+        }
+        try {
+            return crypto
+                .createHmac('sha1',this.salt)
+                .update(password)
+                .digest('hex')
+        } catch (err) {
+            console.error('encryptedPassword',err);
+            return ''
+        }
+    },
+    makeSalt: function () {
+        return Math.round((new Date().valueOf() * Math.random())) + ''
+    }
+}
+ UserSchema.path('hashed_password').validate(function(v) {
+     if (this._password && this._password.length < 6) {
+         this.invalidate('password', 'Password must be at least 6 characters');
+     }
+     if(this.isNew && !this.password) {
+         this.invalidate('password', 'Password is required')
+     }
+ }, null)
+
+export default mongoose.model('User', UserSchema)
