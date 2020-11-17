21d1fad2f3e327b61741c32006a664c35a163a73
Freefireman <beluh.c.s@gmail.com>
2020-11-17 11:40:37 +0200

    starting create styles in Home component

--- client/core/Home.js
+++ client/core/Home.js
@@ -6,6 +6,20 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import unicornbikeImg from './../assets/images/unicornbike.jpg'
 
+const useStyles = makeStyles(theme =>({
+  card: {
+    maxWidth: 600,
+    margin: 'auto',
+    marginTop: theme.spacing(5)
+  },
+  title:{
+    padding: `${theme.spacing(3)}px ${theme.spacing(2.5)}px ${theme.spacing(2)}px`,
+    color: theme.palette.openTitle
+  },
+  media: {
+    minHeight: 400
+  }
+}))
 
 
 const Home = () =>{
